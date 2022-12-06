97704424408fc7e3874306c7e764e8174236d22e
mayconlemosCloud <mayconlemos.riosaude@gmail.com>
2022-12-05 23:54:26 -0300

    teste8

--- main/infra/main.tf
+++ main/infra/main.tf
@@ -21,20 +21,5 @@ provider "aws" {
   }
 }
 
-data "aws_ami" "ubuntu" {
-  owners=["amazon"]
-  most_recent = true
-  name_regex="Ubuntu" 
-}
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name= "${var.environment} : EC2 created by Maycon"
-    Env= var.environment
-    Type= var.instance_type
-   }
-}
 
