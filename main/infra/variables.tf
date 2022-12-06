d815770118fee9528b2d22b44fbfbc7f218f54ea
mayconlemosCloud <mayconlemos.riosaude@gmail.com>
2022-12-05 23:53:07 -0300

    teste8

--- main/infra/variables.tf
+++ main/infra/variables.tf
@@ -1,23 +1,22 @@
 variable "aws_region" {
-    type = string
-    description = "The AWS region"
-    default = "eu-central-1"
-  
+  type        = string
+  description = "The AWS region to deploy to"
+  default     = "eu-central-1"
 }
 
 variable "aws_profile" {
-    type = string
-    description = "The AWS Profile"
-    default = "default"
+  type        = string
+  description = "The AWS profile to use to execute the commands"
+  default     = "default"
 }
 
 variable "environment" {
-    type = string
-    description = "The AWS environment"
-    default = "dev"
+  type        = string
+  description = "The environment to deploy to"
+  default     = "dev"
 }
 
 variable "instance_type" {
-    type = string
-    description = "The AWS instance_type"   
+  type        = string
+  description = "The instance power"
 }
