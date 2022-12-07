ba2849ad0763fb9c2484d6e80328fcf252fba6c4
mayconlemosCloud <mayconlemos.riosaude@gmail.com>
2022-12-06 22:58:13 -0300

    teste6

--- main/infra/config/dev/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-environment="dev"
-instance_type="t2.micro"
--- main/infra/main.tf
+++ main/infra/main.tf
@@ -7,12 +7,21 @@ terraform {
   }
 }
 
+
+resource "aws_instance" "web" {
+  ami           = "ami-0886a3d2c8b721217"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "EC2 created by Maycon"
+  }
+}
+
 provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
+  region = "sa-east-1"
   default_tags {
     tags = {
-      Project    = "Terraform GHA setup"
+      Project    = "Terraform setup"
       CreatedAlt = "2023-12-04"
       ManagedBy  = "Terraform"
       Owner      = "Maycon Lemos"
